1d010df3fa6b97bc7e388ef68382185de4ee3862
Kira Corbin <madkatkeys@gmail.com>
2019-05-29 11:30:45 -0700

    seperated code to javascript and html

---
+++ assets/javascript/game.js
@@ -0,0 +1,62 @@
+// This section will create variables to hold the number of wins, losses, and ties. Most will start at 0.
+
+var wins = 0;
+var losses = 0;
+var ties = 0;
+var counter = 0;
+var maxTires = 3;   //the Max # of tries the user can guess
+
+// This will create variables that hold references to the places in the HTML where we want to display things.
+// const $score = document.getElementById('score');   not sure if const is needed with so many varibles
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var tiesText = document.getElementById("ties-text");
+var maxTires = document.getElementById("max-text");
+
+
+// moved into loop to keep prompting guesses from user. 
+var attempts
+while (attempts != randomLetter) {
+  var attempts = prompt("Please pick a letter between A-Z" + UserNumber);
+  counter += 1;
+
+  // I want to only give 3 tires before counting a loss.
+  if (counter > maxTires) {
+    break
+    document.write("You lose....You have no more tries left.");
+  }
+
+  else (attempts === randomLetter) {
+    document.write("Winner!");
+    document.write("The letter was" + randomLetter);
+    document.write("It took" + counter + "Attempts to get the correct letter");
+  }
+}
+
+// Add the key functions here
+onkeyup = function (event) {
+  console.log(event)
+  console.log(event.key)
+}
+
+// compare to t/f
+console.log(event.key === randomLetter)
+if (event.key === randomLetter) {
+  console.log("RIGHT!! You're awesome!")
+}
+else {
+  console.log("Wrong!! Try again.")
+}
+}
+// Check to see if letter is correct
+// my letter string has been tested with console.log
+var randomLetter = "";
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+  (randomLetter.length < 26); {
+    randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+    }
+// console.log(randomLetter);
+
+
+
+    // Program complete
