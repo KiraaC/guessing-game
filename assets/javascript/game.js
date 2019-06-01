c4aa5e35a82967c26a47f3bc7067999638be0987
Kira Corbin <madkatkeys@gmail.com>
2019-05-31 22:44:39 -0700

    updated javascript render and calling functions, and linked textContent, counter--

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,72 @@
 // This section will create variables to hold the number of wins, losses, and ties. Most will start at 0.
-
+var randomLetter
+var alphabet = "abcdefghijklmnopqrstuvwxyz"
 var wins = 0;
 var losses = 0;
-var ties = 0;
 var counter = 0;
 var maxTries = 3;   //the Max # of tries the user can guess
+var triesLeft = maxTries;
 
 // This will create variables that hold references to the places in the HTML where we want to display things.
 // const $score = document.getElementById('score');   not sure if const is needed with so many varibles
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var tiesText = document.getElementById("ties-text");
-var maxTires = document.getElementById("max-text");
+var maxText = document.getElementById("max-text");
+
+function main() {
+    document.onkeyup = function (event) {
+        console.log(event)
+        console.log(event.key)
+        console.log(event.key === randomLetter)
+        if (event.key === randomLetter) {
+            console.log("RIGHT!! You're awesome!")
+            wins++
+            gameInit()
+        }
+        else {
+            console.log("Wrong!! Try again.")
+            counter++
+            triesLeft--
+            console.log(counter)
+            console.log(maxTries)
+
+            if (counter >= maxTries) {
+                console.log("You lose....You have no more tries left.")
+                losses++
+                gameInit()
+
+            }
+        }
+        render()
+    }
+}
+
+function gameInit() {
+    randomLetter = "";
+    counter = 0;
+    if (randomLetter.length < 26) {
+        randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+    }
+    triesLeft = maxTries
+    console.log(randomLetter)
+}
+
+// defining a function, were assigning a value of the wins variable to the text proprty to the winsText object
+function render(){
+    winsText.textContent = wins
+    lossesText.textContent = losses
+    maxText.textContent = triesLeft
+    // document.getElementById('divA').textContent 
+}
+
+
+// calling functions
+
+gameInit()
+render()
+main()
+
 
 
 // moved into loop to keep prompting guesses from user. 
@@ -34,33 +89,11 @@ var maxTires = document.getElementById("max-text");
 // }
 
 // Add the key functions here
-document.onkeyup = function (event) {
-    console.log(event)
-    console.log(event.key)
-    console.log(event.key === randomLetter)
-    if (event.key === randomLetter) {
-        console.log("RIGHT!! You're awesome!")
-    }
-    else {
-        console.log("Wrong!! Try again.")
-        counter++
-        console.log(counter)
-
-        if (counter >= maxTries) {
-            console.log("You lose....You have no more tries left.")
-        }
-        console.log(maxTries)
-    }
-}
 // Check to see if letter is correct
 // my letter string has been tested with console.log
-var randomLetter = "";
-var alphabet = "abcdefghijklmnopqrstuvwxyz";
-if (randomLetter.length < 26) {
-    randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-}
-console.log(randomLetter)
-
-
-
-    // Program complete
+// var randomLetter = "";
+// var alphabet = "abcdefghijklmnopqrstuvwxyz";
+// if (randomLetter.length < 26) {
+//     randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+// }
+// console.log(randomLetter)
